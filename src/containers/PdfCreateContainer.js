54318316d7a22680b3c66b12feded651e365a20b
Yura <yaxon@tut.by>
2018-12-28 13:56:14 +0300

    add Portal and Context

--- src/containers/PdfCreateContainer.js
+++ src/containers/PdfCreateContainer.js
@@ -1,5 +1,46 @@
-import React, { Component } from 'react';
+import React, { Component, createElement } from 'react';
+import ReactDOM from 'react-dom';
 import PDFPreview from '../Components/function/PDFPreview';
+// import { get } from 'https';
+
+const UserContext = React.createContext('I is on holiday.');
+
+let el = document.createElement('div');
+el.className = 'portal';
+document.getElementsByTagName('div')[0].appendChild(el);
+el = document.getElementsByClassName('portal')[0];
+
+const User = () => (
+  <div>
+    <span>User</span>
+    <InformationUser>
+      <PersonalData />
+    </InformationUser>
+    <UserContext.Consumer>
+      {val => <Commets val={val} />}
+    </UserContext.Consumer>
+  </div>);
+
+class InformationUser extends React.Component {
+   static contextType = UserContext;
+
+   render() {
+     console.log(this.context);
+     return ReactDOM.createPortal(this.props.children, el);
+   }
+}
+
+function PersonalData() {
+  return (<div> Personal data</div>)
+}
+
+// InformationUser.contextType = UserContext;
+
+function Commets(props) {
+  return (
+<div> Commets: {props.val}</div>
+  );
+}
 
 class PdfCreateContainer extends Component {
   state = {
@@ -21,10 +62,13 @@ class PdfCreateContainer extends Component {
           type='text'
           onChange={this.handleNameChange}
         />
-
         <button onClick={this.handleClick}>Generate PDF</button>
         {this.state.showPDFPreview && <PDFPreview title={greeting} />}
-      </div>
+
+        <UserContext.Provider value={'I work.'}>
+          <User />
+        </UserContext.Provider>
+</div>
     );
   }
 }
